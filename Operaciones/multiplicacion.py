75de23c75ed982de65b851607674271914c3ac15
Ismaelp08 <103200651+Ismaelp08@users.noreply.github.com>
2022-04-07 11:38:07 -0400

    Add files via upload

---
+++ Operaciones/multiplicacion.py
@@ -0,0 +1,6 @@
+valor1=int(input ("Digite el primer numero: "))
+valor2=int(input ("Digite el segundo numero: "))
+
+resultado = valor1 * valor2
+
+print (f"El reultado es: {resultado}") 
---
+++ Operaciones/suma.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+
+valor1=int(input ("Digite el primer numero: "))
+valor2=int(input ("Digite el segundo numero: "))
+
+resultado = valor1 + valor2
+
+print (f"El reultado es: {resultado}") 
+
+window.mainloop()
